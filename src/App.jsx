936f2a26f0be085c636d69aebc2b8c6936d98ebf
varunvarshney00 <varunvarshney6882@gmail.com>
2024-05-01 05:41:59 +0530

    first commit candidate application portal

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import JobCard from "./components/JobCard";
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <SearchBar/>
+      <JobCard/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='mt-10 flex flex-col gap-5 items-center justify-center text-black'>
+      <h1 className='text-5xl font-bold'>Job dhundne aaye ho bhai?</h1>
+      <p className='text-xl'>Zaroor milegi, ek bar hamare portal ko try krke toh dekho!!</p>
+    </div>
+  )
+}
+
+export default Header
