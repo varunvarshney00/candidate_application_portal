b2b0a499b545217e535b3da6afc24905806bf439
varunvarshney00 <varunvarshney6882@gmail.com>
2024-05-02 04:43:42 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-
 import Header from "./components/Header";
-
-
-
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div>
-  
+      <Navbar/>
       <Header/>
-
+      
     </div>
   )
 }
