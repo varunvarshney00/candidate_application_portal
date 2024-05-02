6af1ef54c21f607bb87175889f14bed6923b407d
varunvarshney00 <varunvarshney6882@gmail.com>
2024-05-02 21:40:28 +0530

    heading changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,9 @@
 function Header() {
   return (
     <div className='t-10 sm:mt-20 lg:mt-20 flex flex-col items-center justify-center text-black'>
-      <h1 className='text-4xl sm:text-5xl lg:text-6xl font-bold text-center mb-5'>Job dhundne aaye ho bhai?</h1>
+      <h1 className='text-4xl sm:text-5xl lg:text-6xl font-bold text-center mb-5'>Candidate Application Portal</h1>
       
-      <p className='text-lg sm:text-xl lg:text-2xl text-center'>Zaroor milegi, ek bar hamare portal ko try krke toh dekho!!</p>
+      <p className='text-lg sm:text-xl lg:text-2xl text-center'>Unlock Your Career Potential with our Candidate Application Portal – Where Opportunities Meet Ambition!</p>
     </div>
   )
 }
